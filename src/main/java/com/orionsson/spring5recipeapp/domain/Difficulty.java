10ba0eeeef9aa83a60237ab2e3bfbf5cd71d19f8
fatma arik copur <fatmaarik@gmail.com>
2018-12-14 17:57:36 -0500

    enumaration type is added.

---
+++ src/main/java/com/orionsson/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.orionsson.spring5recipeapp.domain;
+
+public enum Difficulty {
+    EASY,MODERATE,HARD
+}
--- src/main/java/com/orionsson/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/orionsson/spring5recipeapp/domain/Recipe.java
@@ -1,7 +1,5 @@
 package com.orionsson.spring5recipeapp.domain;
 
-import org.hibernate.annotations.Cascade;
-
 import javax.persistence.*;
 import java.util.Set;
 
@@ -27,6 +25,8 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL ,mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -106,5 +106,21 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
