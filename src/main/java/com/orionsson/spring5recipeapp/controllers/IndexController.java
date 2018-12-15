49895590c64aaebf80b08496d5e1c0932e4fe235
fatma arik copur <fatmaarik@gmail.com>
2018-12-15 02:25:31 -0500

    optional element is added index controller.

--- src/main/java/com/orionsson/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/orionsson/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,30 @@
 package com.orionsson.spring5recipeapp.controllers;
 
-import org.springframework.stereotype.Component;
+import com.orionsson.spring5recipeapp.domain.Category;
+import com.orionsson.spring5recipeapp.domain.UnitOfMeasure;
+import com.orionsson.spring5recipeapp.repositories.CategoryRepository;
+import com.orionsson.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
-public class IndexController {
+import java.util.Optional;
 
-    @RequestMapping({"","/","index.html"})
+@Controller
+public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"","/","/index"})
     public String getIndex(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("Teaspoon");
+        System.out.println("The cat ID is " + categoryOptional.get().getId());
+        System.out.println("The uom ID is " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/orionsson/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/orionsson/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.orionsson.spring5recipeapp.repositories;
 import com.orionsson.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+    Optional<UnitOfMeasure> findByUom(String uom);
 }
