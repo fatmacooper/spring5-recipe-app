8850868d2f5c4b1d5b4d5c41de916621a1a2f17f
fatma arik copur <fatmaarik@gmail.com>
2018-12-18 18:54:03 -0500

    Integration test is developed.

---
+++ src/test/java/com/orionsson/spring5recipeapp/domain/UnitOfMeasureTestIT.java
@@ -0,0 +1,31 @@
+package com.orionsson.spring5recipeapp.domain;
+
+import com.orionsson.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureTestIT {
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    //@DirtiesContext
+    public void findByDescription() throws Exception{
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByUom("Teaspoon");
+        assertEquals("Teaspoon",uomOptional.get().getUom());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception{
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByUom("Cup");
+        assertEquals("Cup",uomOptional.get().getUom());
+    }
+}
