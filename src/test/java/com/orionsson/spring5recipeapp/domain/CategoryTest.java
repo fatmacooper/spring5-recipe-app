023186e8198e381e26f9896d6f8e761ec93dae15
fatma arik copur <fatmaarik@gmail.com>
2018-12-18 15:15:46 -0500

    category test is developed.

---
+++ src/test/java/com/orionsson/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,28 @@
+package com.orionsson.spring5recipeapp.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+    private Category category;
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+    @Test
+    public void getId() throws Exception{
+        Long id = 4l;
+        category.setId(id);
+        assertEquals(id,category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception{
+    }
+
+    @Test
+    public void getRecipes() throws Exception{
+    }
+}
