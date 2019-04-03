2bc691fc9c097b3bd1746b39ffb2182d2c0d5f67
fatma arik copur <fatmaarik@gmail.com>
2019-04-03 14:10:10 -0400

    ImageService is reactivated.

--- src/main/java/com/orionsson/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/orionsson/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,39 +1,43 @@
 package com.orionsson.spring5recipeapp.services;
 
 import com.orionsson.spring5recipeapp.domain.Recipe;
-import com.orionsson.spring5recipeapp.repositories.RecipeRepository;
+import com.orionsson.spring5recipeapp.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
-    private final RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeReactiveRepository;
 
-    public ImageServiceImpl(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
+    public ImageServiceImpl(RecipeReactiveRepository recipeReactiveRepository) {
+        this.recipeReactiveRepository = recipeReactiveRepository;
     }
 
-    @Transactional
     @Override
-    public void saveImageFile(String recipeId, MultipartFile file) {
-        try{
-            Recipe recipe = recipeRepository.findById(recipeId).get();
-            Byte[] byteObjects = new Byte[file.getBytes().length];
-            int i = 0;
-            for(byte b: file.getBytes()){
-                byteObjects[i++] = b;
-            }
-            recipe.setImage(byteObjects);
-            recipeRepository.save(recipe);
+    public Mono<Void> saveImageFile(String recipeId, MultipartFile file) {
 
-        }catch(IOException e){
-            log.debug("Error occured",e);
-            e.printStackTrace();
-        }
+            Mono<Recipe> recipeMono = recipeReactiveRepository.findById(recipeId).map(recipe -> {
+                try{
+                    Byte[] byteObjects = new Byte[file.getBytes().length];
+                    int i = 0;
+                    for(byte b: file.getBytes()){
+                        byteObjects[i++] = b;
+                    }
+                    recipe.setImage(byteObjects);
+                    return  recipe;
+                }catch(IOException e){
+                    log.debug("Error occured",e);
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            });
+
+        recipeReactiveRepository.save(recipeMono.block()).block();
+        return Mono.empty();
     }
 }
--- src/test/java/com/orionsson/spring5recipeapp/services/ImageServiceImplTest.java
+++ src/test/java/com/orionsson/spring5recipeapp/services/ImageServiceImplTest.java
@@ -1,7 +1,7 @@
 package com.orionsson.spring5recipeapp.services;
 
 import com.orionsson.spring5recipeapp.domain.Recipe;
-import com.orionsson.spring5recipeapp.repositories.RecipeRepository;
+import com.orionsson.spring5recipeapp.repositories.reactive.RecipeReactiveRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -9,22 +9,21 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Optional;
+import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class ImageServiceImplTest {
     @Mock
-    RecipeRepository recipeRepository;
+    RecipeReactiveRepository recipeReactiveRepository;
 
     ImageService imageService;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        imageService = new ImageServiceImpl(recipeRepository);
+        imageService = new ImageServiceImpl(recipeReactiveRepository);
     }
 
     @Test
@@ -35,8 +34,8 @@ public class ImageServiceImplTest {
                 "text/plain","Orions Son".getBytes());
         Recipe recipe = new Recipe();
         recipe.setId(id);
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
-        when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+        when(recipeReactiveRepository.findById(anyString())).thenReturn(Mono.just(recipe));
+        when(recipeReactiveRepository.save(any(Recipe.class))).thenReturn(Mono.just(recipe));
 
         ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
 
@@ -44,7 +43,7 @@ public class ImageServiceImplTest {
         imageService.saveImageFile(id,multipartFile);
 
         //then
-        verify(recipeRepository,times(1)).save(argumentCaptor.capture());
+        verify(recipeReactiveRepository,times(1)).save(argumentCaptor.capture());
         Recipe savedRecipe = argumentCaptor.getValue();
         assertEquals(multipartFile.getBytes().length,savedRecipe.getImage().length);
     }
